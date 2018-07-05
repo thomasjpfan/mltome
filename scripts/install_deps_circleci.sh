84e79e4714f23628ca0efacc97ac75247c9f7584
Thomas Fan <thomasjpfan@gmail.com>
2018-07-05 18:32:05 -0400

    TST: No progress bars

--- scripts/install_deps_circleci.sh
+++ scripts/install_deps_circleci.sh
@@ -1,2 +1,2 @@
-pip install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
-pip install -e .[dev]
+pip install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp36-cp36m-linux_x86_64.whl --progress-bar off
+pip install -e .[dev] --progress-bar off
