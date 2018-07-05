e500444ee2c0214bc618dfce43ff1f1f5a58c943
Thomas Fan <thomasjpfan@gmail.com>
2018-07-05 19:38:33 -0400

    TST: Fix cache tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -5,14 +5,11 @@ import pandas as pd
 from mltome.cache import from_dataframe_cache
 
 
-def get_target():
-    @from_dataframe_cache('target')
-    def output(params, force=False, **kwargs):
-        multiplier = params['multiplier']
-        df = pd.DataFrame({"a": [1, 2, 3]})
-        return multiplier * df
-
-    return output
+@from_dataframe_cache('target')
+def get_target(params, force=False, **kwargs):
+    multiplier = params['multiplier']
+    df = pd.DataFrame({"a": [1, 2, 3]})
+    return multiplier * df
 
 
 def test_dataframe_cache_saves_to_cache(tmpdir):
@@ -24,22 +21,22 @@ def test_dataframe_cache_saves_to_cache(tmpdir):
     assert output.equals(expected_df)
     assert target.exists()
 
-    # before_mtime = target.stat().st_mtime
-    # output = get_target(param)
-    # after_mtime = target.stat().st_mtime
-    # assert before_mtime == after_mtime
+    before_mtime = target.stat().st_mtime
+    output = get_target(param)
+    after_mtime = target.stat().st_mtime
+    assert before_mtime == after_mtime
 
 
-# def test_dataframe_cache_updates_cache_with_force(tmpdir, get_target):
-    # target = Path(tmpdir.mkdir('cache').join('output.parq'))
-    # param = {'target': target, 'multiplier': 2}
+def test_dataframe_cache_updates_cache_with_force(tmpdir):
+    target = Path(tmpdir.mkdir('cache').join('output2.parq'))
+    param = {'target': target, 'multiplier': 2}
 
-    # expected_df = pd.DataFrame({"a": [2, 4, 6]})
-    # output = get_target(param)
-    # assert output.equals(expected_df)
-    # assert target.exists()
+    expected_df = pd.DataFrame({"a": [2, 4, 6]})
+    output = get_target(param)
+    assert output.equals(expected_df)
+    assert target.exists()
 
-    # before_mtime = target.stat().st_mtime
-    # output = get_target(param, force=True)
-    # after_mtime = target.stat().st_mtime
-    # assert before_mtime != after_mtime
+    before_mtime = target.stat().st_mtime
+    output = get_target(param, force=True)
+    after_mtime = target.stat().st_mtime
+    assert before_mtime != after_mtime
