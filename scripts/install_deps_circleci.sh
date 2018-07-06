affb1905cec8127618c0ec9489534d85ab31fde2
Thomas Fan <thomasjpfan@gmail.com>
2018-07-05 23:37:40 -0400

    TST: Fix

--- scripts/install_deps_circleci.sh
+++ scripts/install_deps_circleci.sh
@@ -1,4 +1,4 @@
 sudo apt-get install libsnappy-dev
 pip install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp36-cp36m-linux_x86_64.whl --progress-bar off
 pip install -e .[dev] --progress-bar off
-pip install python-snappy -I
+pip install python-snappy pyarrow -I --progress-bar off
