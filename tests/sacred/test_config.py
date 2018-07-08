a1eba89e3f2acb716b0f00107341fe41615575c0
Thomas Fan <thomasjpfan@gmail.com>
2018-07-08 17:38:44 -0400

    TST: More tests

--- tests/sacred/test_config.py
+++ tests/sacred/test_config.py
@@ -1,13 +1,21 @@
+import logging
+
 from sacred import Experiment
 
-from mltome.sacred.config import add_common_config
+from mltome.sacred.config import add_common_config, add_monogodb
 
 
 def test_add_common_config(tmpdir):
     csv_fn = str(tmpdir.mkdir('artifacts').join('results.csv'))
 
-    exp = Experiment("test")
+    exp = Experiment('test')
 
     add_common_config(exp, csv_fn)
 
     assert len(exp.observers) == 2
+
+
+def test_add_monogodb():
+    obs = []
+    add_monogodb(obs, 'url', 'name')
+    assert len(obs) == 1
