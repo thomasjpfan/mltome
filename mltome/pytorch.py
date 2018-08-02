e04c8b59a485d863b0d9e7a99475bf87942b9bb3
Thomas Fan <thomasjpfan@gmail.com>
2018-08-02 11:46:16 -0400

    RFC: Remove simulate_lr

--- mltome/pytorch.py
+++ mltome/pytorch.py
@@ -1,7 +1,4 @@
 """Pytorch utilities"""
-import numpy as np
-import torch
-import matplotlib.pyplot as plt
 
 
 def set_requires_grad(module, name, value):
@@ -24,44 +21,3 @@ def set_requires_grad(module, name, value):
                      if n.startswith(name))
     for p in filter_params:
         p.requires_grad_(value)
-
-
-def simulate_lrs(lr_sch_class, steps, initial_lr, **kwargs):
-    """
-    Simulates the learning rate scheduler.
-
-    Parameters
-    ----------
-    lr_sch_class: class
-        Class of pytorch learning rate scheduler
-
-    steps: int
-        Number of steps to simulate
-
-    initial_lr: float
-        Initial learning rate
-
-    Returns
-    -------
-    numpy array
-
-    """
-    test = torch.ones(1, requires_grad=True)
-    opt = torch.optim.SGD([{'params': test, 'lr': initial_lr}])
-    sch = lr_sch_class(opt, **kwargs)
-
-    has_batch_step = (hasattr(lr_sch_class, 'batch_step')
-                      and callable(lr_sch_class.batch_step))
-    lrs = []
-    for _ in range(steps):
-        sch.batch_step() if has_batch_step else sch.step()
-        lrs.append(sch.get_lr()[0])
-
-    return np.array(lrs)
-
-
-def plot_lrs(lr_sch_class, steps, by_epoch, ax=None, **kwargs):
-    if ax is None:
-        _, ax = plt.subplots()
-    lrs = simulate_lrs(lr_sch_class, steps, by_epoch, **kwargs)
-    ax.plot(lrs)
