68647cca4ff395ec6d9291080eaca82672e541e3
Thomas Fan <thomasjpfan@gmail.com>
2018-09-29 17:37:36 -0400

    TST: Fix

--- tests/test_neptune.py
+++ tests/test_neptune.py
@@ -1,28 +1,6 @@
 from unittest.mock import NonCallableMagicMock, call
 
-from mltome.neptune import NeptuneObserver, NeptuneSkorchCallback
-
-
-def test_neptuneobserver():
-    ctx_mock = NonCallableMagicMock()
-    no = NeptuneObserver(ctx_mock, 'model_id', 'tags')
-
-    no.started_event(None, 'train', None, None, {
-        'model_id': 'id1',
-        'tags': ['1', '2']
-    }, None, None)
-
-    ctx_mock.properties.__setitem__.assert_called_with('model_id', 'id1_train')
-    ctx_mock.tags.append.assert_has_calls(
-        [call('1'), call('2')], any_order=True)
-
-    no.completed_event(None, [1])
-    assert not ctx_mock.channel_send.call_count
-
-    no.completed_event(None, [1, 2])
-
-    ctx_mock.channel_send.assert_has_calls(
-        [call('valid', 0, 1), call('train', 0, 2)], any_order=True)
+from mltome.neptune import NeptuneSkorchCallback
 
 
 def test_neptunecallback():
