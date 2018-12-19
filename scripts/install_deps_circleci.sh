4c03138978f565ce43cbb222371a7305dc9ce4b3
Thomas Fan <thomasjpfan@gmail.com>
2018-12-18 19:43:32 -0500

    TST: Adds python 3.7

--- scripts/install_deps_circleci.sh
+++ scripts/install_deps_circleci.sh
@@ -1,2 +1,2 @@
-pip install https://download.pytorch.org/whl/cpu/torch-1.0.0-cp36-cp36m-linux_x86_64.whl --progress-bar off
+pip install $PYTORCH_WHEEL --progress-bar off
 pip install -e .[dev] --progress-bar off
